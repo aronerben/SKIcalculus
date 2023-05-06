e3392edb1172d7a9ed611dc206556b654b20a549
aronerben <aron.erben@gmail.com>
2023-05-06 15:03:48 +0200

    Add working mininmal formula algorithm

--- src/Core2.hs
+++ src/Core2.hs
@@ -2,9 +2,13 @@
 
 module Core2 where
 
+import Control.Monad (guard)
 import Core (SKI (..))
+import Data.Either (isLeft)
 import Data.Function (on, (&))
-import Data.List (minimumBy, partition, sortBy)
+import Data.List (deleteBy, find, foldl', minimumBy, partition, sortBy)
+import Data.Map
+import Debug.Trace (trace)
 import Nat (ChurchNumeral, add', exp', mul')
 import Text.Printf (PrintfArg, PrintfType, printf)
 
@@ -115,100 +119,100 @@ import Text.Printf (PrintfArg, PrintfType, printf)
 -- -- general (AbsF _ (n, _)) = error $ "abs" <> show n
 
 -- TODO add Succ
-data Expr
-  = (:+) Expr Expr
-  | (:*) Expr Expr
-  | (:^) Expr Expr
-  | Val Integer
+-- data Expr
+--   = (:+) Expr Expr
+--   | (:*) Expr Expr
+--   | (:^) Expr Expr
+--   | Val Integer
 
-print' ::
-  (PrintfArg t1, PrintfType t2, Show a1, Show a2) =>
-  a1 ->
-  t1 ->
-  a2 ->
-  t2
-print' l op r = printf "(%s %s %s)" (show l) op (show r)
+-- print' ::
+--   (PrintfArg t1, PrintfType t2, Show a1, Show a2) =>
+--   a1 ->
+--   t1 ->
+--   a2 ->
+--   t2
+-- print' l op r = printf "(%s %s %s)" (show l) op (show r)
 
-instance Show Expr where
-  show (a :+ b) = print' a ("+" :: String) b
-  show (a :* b) = print' a ("*" :: String) b
-  show (a :^ b) = print' a ("^" :: String) b
-  show (Val i) = show i
+-- instance Show Expr where
+--   show (a :+ b) = print' a ("+" :: String) b
+--   show (a :* b) = print' a ("*" :: String) b
+--   show (a :^ b) = print' a ("^" :: String) b
+--   show (Val i) = show i
 
--- Do not give precedence on purpose, want left to right evaluation
+-- -- Do not give precedence on purpose, want left to right evaluation
 
-extract :: Expr -> Integer
-extract (a :+ b) = extract a + extract b
-extract (a :* b) = extract a * extract b
-extract (a :^ b) = extract a ^ extract b
-extract (Val i) = i
+-- extract :: Expr -> Integer
+-- extract (a :+ b) = extract a + extract b
+-- extract (a :* b) = extract a * extract b
+-- extract (a :^ b) = extract a ^ extract b
+-- extract (Val i) = i
 
-rules :: Expr -> Bool
-rules (Val 0 :* _) = False
-rules (Val 1 :* _) = False
-rules (_ :* Val 1) = False
-rules (Val 0 :^ _) = False
-rules (Val 1 :^ _) = False
-rules (_ :^ Val 1) = False
-rules _ = True
+-- rules :: Expr -> Bool
+-- rules (Val 0 :* _) = False
+-- rules (Val 1 :* _) = False
+-- rules (_ :* Val 1) = False
+-- rules (Val 0 :^ _) = False
+-- rules (Val 1 :^ _) = False
+-- rules (_ :^ Val 1) = False
+-- rules _ = True
 
-vals :: [Integer]
--- TODO this is limited
-vals = reverse [1 .. 11]
+-- vals :: [Integer]
+-- -- TODO this is limited
+-- vals = reverse [1 .. 11]
 
-type Op = Expr -> Expr -> Expr
+-- type Op = Expr -> Expr -> Expr
 
-ops :: [Op]
-ops = [(:+), (:*), (:^)]
+-- ops :: [Op]
+-- ops = [(:+), (:*), (:^)]
 
-raw ::
-  (Integer, Expr) ->
-  Integer ->
-  Op ->
-  Integer ->
-  Integer ->
-  [(Integer, Expr)]
-raw acc val op n len
-  | res >= n || len == 0 = [acc']
-  | otherwise =
-    concat
-      [ raw acc' val' op' n (len - 1)
-      | op' <- ops
-      , -- TODO WRONG
-      -- , val' <- if op' == Exp then vals else dropWhile (> val) vals
-      -- TODO maybe check for current sum what the min weight is, if curr is higher, abort
-      val' <- vals
-      , rules $ op' (Val $ fst acc) (Val val')
-      ]
-  where
-    entry = op (Val $ fst acc) (Val val)
-    res = extract entry
-    acc' = (res, op (snd acc) (Val val))
+-- raw ::
+--   (Integer, Expr) ->
+--   Integer ->
+--   Op ->
+--   Integer ->
+--   Integer ->
+--   [(Integer, Expr)]
+-- raw acc val op n len
+--   | res >= n || len == 0 = [acc']
+--   | otherwise =
+--       concat
+--         [ raw acc' val' op' n (len - 1)
+--           | op' <- ops,
+--             -- TODO WRONG
+--             -- , val' <- if op' == Exp then vals else dropWhile (> val) vals
+--             -- TODO maybe check for current sum what the min weight is, if curr is higher, abort
+--             val' <- vals,
+--             rules $ op' (Val $ fst acc) (Val val')
+--         ]
+--   where
+--     entry = op (Val $ fst acc) (Val val)
+--     res = extract entry
+--     acc' = (res, op (snd acc) (Val val))
 
-run :: Integer -> Integer -> [(Integer, Expr)]
-run n maxOps =
-  Prelude.filter ((== n) . fst) $
-    concat
-      [ raw (start, Val start) val' op' n (maxOps - 1)
-      | val' <- vals
-      , op' <- ops
-      , start <- vals
-      , rules $ op' (Val start) (Val val')
-      ]
+-- run :: Integer -> Integer -> [(Integer, Expr)]
+-- run n maxOps =
+--   Prelude.filter ((== n) . fst) $
+--     concat
+--       [ raw (start, Val start) val' op' n (maxOps - 1)
+--         | val' <- vals,
+--           op' <- ops,
+--           start <- vals,
+--           rules $ op' (Val start) (Val val')
+--       ]
 
-weight :: Expr -> Integer
-weight (a :+ b) = weight a + weight b + 39
-weight (a :* b) = weight a + weight b + 19
-weight (a :^ b) = weight a + weight b + 23
--- --          Fix Succs      Parens    Spaces
--- --          V   V          V         V
-weight (Val i) = 5 + i * 17 + i * 2 + i
+-- weight :: Expr -> Integer
+-- weight (a :+ b) = weight a + weight b + 39
+-- weight (a :* b) = weight a + weight b + 19
+-- weight (a :^ b) = weight a + weight b + 23
+-- -- --        Fix Succs      Parens    Spaces
+-- -- --        V   V          V         V
+-- weight (Val i) = 5 + i * 17 + i * 2 + i
 
-min10 :: Integer -> Integer -> [(Integer, Expr)]
-min10 n maxOps = Prelude.take 10 $ sortBy (\(_, e1) (_, e2) -> compare (weight e1) (weight e2)) $ run n maxOps
+-- min10 :: Integer -> Integer -> [(Integer, Expr)]
+-- min10 n maxOps = Prelude.take 10 $ sortBy (\(_, e1) (_, e2) -> compare (weight e1) (weight e2)) $ run n maxOps
 
-lightest :: Integer -> Integer -> Expr
-lightest n maxOps = snd $ minimumBy (\(_, e1) (_, e2) -> compare (weight e1) (weight e2)) $ run n maxOps
+-- lightest :: Integer -> Integer -> Expr
+-- lightest n maxOps = snd $ minimumBy (\(_, e1) (_, e2) -> compare (weight e1) (weight e2)) $ run n maxOps
 
 -- skify :: Expr -> SKI (ChurchNumeral Integer)
 -- skify (a :+ b) = add' :- skify a :- skify b
@@ -223,48 +227,58 @@ lightest n maxOps = snd $ minimumBy (\(_, e1) (_, e2) -> compare (weight e1) (we
 -- expr, val of expr, weight of expr
 -- type Dict = [(Expr, Int, Int)]
 -- nr and # ops and fixed
-type Amount = Integer
+-- type Amount = Integer
 
-type Value = Integer
+-- type Value = Integer
 
-type Fixed = Bool
+-- type Fixed = Bool
 
-type Dict = [((Value, Amount), Fixed)]
+-- type Dict = [(((Value, Amount), String), Fixed)]
 
-ops' :: [Value -> Value -> Value]
-ops' = [(+), (*), (^), flip (^)]
+-- ops' :: [(Value -> Value -> Value, String)]
+-- ops' = [((+), "+"), ((*), "*"), ((^), "^"), (flip (^), "^")]
 
-goal :: Value
-goal = 4096
+-- goal :: Value
+-- goal = 20
 
-step :: Dict -> Either Dict Dict
-step dict = do
-  ((smol, amt), dict') <- least dict
-  -- TODO stop condition if all overshot
-  let new = [((val `op` smol, amount + amt + 1), False) | op <- ops', ((val, amount), f) <- dict', f]
-  return $ dict' <> new
+-- step :: Dict -> Either Dict Dict
+-- step dict = do
+--   (((smol, amt), expry), dict') <- least dict
+--   -- TODO stop condition if all overshot
+--   let new =
+--         [ (((val `op` smol, amount + amt + 1), "(" <> expr <> repr <> expry <> ")"), False)
+--           | (op, repr) <- ops',
+--             (((val, amount), expr), f) <- dict',
+--             f,
+--             val `op` smol <= goal
+--         ]
+--   return $ dict' <> new
 
-least :: Dict -> Either Dict ((Value, Amount), Dict)
-least dict =
-  case sorted of
-    ((v, amt), _) : _ ->
-      if v == goal
-        then Left $ res
-        else return ((v, amt), res)
-      where
-        res = swap v amt
-    _ -> error "Empty list"
-  where
-    (unfixed, fixed) =
-      partition (not . snd) dict
-    sorted =
-      sortBy (compare `on` (snd . fst)) unfixed
-    swap v op = fixed ++ ((v, op), True) : tail unfixed
+-- least :: Dict -> Either Dict (((Value, Amount), String), Dict)
+-- least dict =
+--   case sorted of
+--     (((v, amt), expr), _) : _ ->
+--       if v == goal
+--         then Left res
+--         else return (((v, amt), expr), res)
+--       where
+--         res = swap v amt expr
+--     _ -> error "Empty list"
+--   where
+--     (unfixed, fixed) =
+--       partition (not . snd) dict
+--     sorted =
+--       sortBy (compare `on` (snd . fst . fst)) unfixed
+--     swap v op expr = fixed ++ (((v, op), expr), True) : tail unfixed
 
-run' :: [Integer] -> Either Dict Dict
-run' nrs = go $ (\nr -> ((nr, 0), False)) <$> nrs
-  where
-    go v = step v >>= go
+-- run' :: [Integer] -> Either Dict Dict
+-- run' nrs = go $ (\nr -> (((nr, 0), show nr), False)) <$> nrs
+--   where
+--     go v = step v >>= go
+
+-- findIt (Left xs) = head $ reverse $ filter snd xs
+
+-- heady (Left xs) = take 100 xs
 
 -- & head
 -- & minimumBy (compare `on` (snd . fst))
@@ -285,3 +299,83 @@ run' nrs = go $ (\nr -> ((nr, 0), False)) <$> nrs
 
 -- least :: Dict -> Integer
 -- least dict = minimumBy (comparing fst) dict
+
+-- weight :: Expr -> Integer
+-- weight (a :+ b) = weight a + weight b + 39
+-- weight (a :* b) = weight a + weight b + 19
+-- weight (a :^ b) = weight a + weight b + 23
+-- -- --            Fix Succs    Parens  Spaces
+-- -- --            V   V        V       V
+-- weight (Val i) = 5 + i * 17 + i * 2 + i
+
+type Value = Integer
+
+type Length = Integer
+
+type Repr = String
+
+type Entry = (Value, (Length, Repr))
+
+-- type Dict = Map Value (Length, Repr)
+type Dict = [Entry]
+
+ops :: [(Value -> Value -> Value, String)]
+ops =
+  [ ((+), "+")
+  , ((*), "*")
+  , ((^), "^")
+  , (flip (^), "^")
+  ]
+
+-- Insert according to a comparator, assume the list is sorted, else unpredictable results
+-- Good enough for this use case as we build up the list with this function
+insertSortedBy :: (a -> a -> Bool) -> a -> [a] -> [a]
+insertSortedBy cmp el (hd : tl)
+  | cmp el hd = el : hd : tl
+  | otherwise = hd : insertSortedBy cmp el tl
+insertSortedBy _ el [] = [el]
+
+-- TODO unfixed should be a sorted map for better performance
+upsertUnfixed :: Dict -> Entry -> Dict
+upsertUnfixed unfixed entry@(val, (len, _)) =
+  updateUnfixed $ insertSortedBy (\(_, (len1, _)) (_, (len2, _)) -> len1 < len2) entry
+  where
+    updateUnfixed inserter = case find (\(curEntryVal, _) -> curEntryVal == val) unfixed of
+      Just (_, (curEntryLength, _)) ->
+        if curEntryLength > len
+          then -- Element found in Dict that has worse length (longer)
+          -- => Delete it and insert better entry
+            deleteBy (\(v1, _) (v2, _) -> v1 == v2) entry unfixed & inserter
+          else -- Element found in Dict that has better length
+            unfixed
+      -- Element not found => Insert it
+      Nothing -> unfixed & inserter
+
+step :: Value -> (Dict, Dict) -> (Dict, Dict)
+step goal (fixed@((minEntryVal, (minEntryLength, minEntryRepr)) : _), unfixed) =
+  let unfixedNew = Data.List.foldl' updatedUnfixed unfixed cartesian
+   in (fixed, unfixedNew)
+  where
+    cartesian = do
+      op <- ops
+      entry <- fixed
+      return (op, entry)
+    updatedUnfixed unfixedNew ((op, repr), (curEntryVal, (curEntryLength, curEntryRepr))) = do
+      let newVal = curEntryVal `op` minEntryVal
+      let newLength = curEntryLength + minEntryLength + 1
+      let newRepr = "(" <> curEntryRepr <> repr <> minEntryRepr <> ")"
+      let newEntry = (newVal, (newLength, newRepr))
+      -- This prevents sub and div from working, need other way to stop early
+      if newVal > goal then unfixedNew else upsertUnfixed unfixedNew newEntry
+step _ _ = error "Empty list yo"
+
+run' :: Value -> [Value] -> (Dict, Dict)
+run' goal nrs = go ([], start)
+  where
+    start = (\nr -> (nr, (0, show nr))) <$> nrs
+    go (fixed, hd@(v, _) : unfixed)
+      | v == goal = partitions
+      | otherwise = go $ step goal partitions
+      where
+        partitions = (hd : fixed, unfixed)
+    go _ = error $ "Could not get to " <> show goal <> " with " <> show nrs
